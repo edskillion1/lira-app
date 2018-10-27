6b207a3de9952e9e5303cff8bfbd3e649cf2ed46
Ed Skillion <edskillion@outlook.com>
2018-10-27 01:02:34 -0700

    lira assignment

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('twitterKeys are loaded');
+
+exports.twitterKeysSource = {
+  consumer_key: 'rx8JsjFjuY2yCx0jemVtx4dth',
+  consumer_secret: 'emBy5jBDWT4PQzkop7kvF4pCuIdicsnXg5RCQLiRG5CQ5DMQnX',
+  access_token_key: '184956482-YvpiXNlyLVNohwFKaKt4vcUg1CWUNzgRg7YYeIvu',
+  access_token_secret: 'wBQhXAmfmvqAIwybeC4G7fLAEMIIXZBcp1fVM7PXCDvZY'
+}
+
+//exports.spotifyKeys = {
+//   Client ID: '586b6d05e19c4029bbcaaf030c3eda9a',
+//   Client Secret: 'deed7e1281004d5681fbed376cf5a6e9'
+//}
